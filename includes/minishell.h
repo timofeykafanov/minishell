1cbe3351adc158fd497f17b03026952d2ecfe42e
Silas Oppermann <sopperma@c4r3p3.42vienna.com>
2024-07-28 11:49:12 +0200

    lexer

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,17 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tkafanov <tkafanov@student.42vienna.com    +#+  +:+       +#+        */
+/*   By: sopperma <sopperma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/26 11:29:57 by tkafanov          #+#    #+#             */
-/*   Updated: 2024/07/26 11:31:29 by tkafanov         ###   ########.fr       */
+/*   Updated: 2024/07/27 12:46:23 by sopperma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+#include <stdlib.h>
+
 # define SUCCESS 0
 # define ERROR 1
+
+# define ECHO "echo"
+# define CD "cd"
+# define PWD "pwd"
+# define EXPORT "export"
+# define UNSET "unset"
+# define ENV "env"
+# define EXIT "exit"
+
+# define R_IN "<"
+# define R_OUT ">"
+# define FIND_DELIMITER "<<"
+# define OUT_APPEND ">>"
+# define PIPE "|"
+# define DOLLAR "$"
+
+# define T_WORD 1
+# define T_PIPE 2
+# define T_RET_IN 3 // <
+# define T_RET_OUT 4 // >
+
+
+typedef struct s_tokens
+{
+    void            *data;
+    int             type;
+    struct s_tokens *next;
+}   t_tokens;
+
+
+typedef struct s_memory
+{
+    struct s_tokens *tokens;
+}   t_memory;
+
+
+
 
 #endif // MINISHELL_H
--- srcs/main.c
+++ srcs/main.c
@@ -3,16 +3,86 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tkafanov <tkafanov@student.42vienna.com    +#+  +:+       +#+        */
+/*   By: sopperma <sopperma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/26 11:29:22 by tkafanov          #+#    #+#             */
-/*   Updated: 2024/07/26 11:31:49 by tkafanov         ###   ########.fr       */
+/*   Updated: 2024/07/28 11:48:38 by sopperma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	main(void)
+
+int get_type(char *av)
 {
+	if (*av == R_IN)
+		return (T_RET_IN);
+	else if (*av == R_OUT)
+		return (T_RET_OUT);
+	else if (*av == PIPE)
+		return (T_PIPE);
+	else
+		return (T_WORD);
+}
+
+void free_tokens(t_tokens *token)
+{
+	t_tokens *current;
+	
+	while(current)
+	{
+		current = token->next;
+		free(token->data);
+		free(token);
+		token = current;
+	}
+}
+
+void free_memory(t_memory *memory)
+{
+	if (memory->tokens)
+		free_tokens(memory->tokens);
+}
+
+t_tokens	*create_token(char *av, t_memory *memory)
+{
+	t_tokens *token;
+	
+	token = malloc(sizeof(t_tokens));
+	if(!token)
+		return (free_memory(memory), NULL);
+	token->data = av;
+	token->type = get_type(av);
+	return (token);
+}
+
+int	*lexer(int ac, char **av, t_memory *memory)
+{
+	t_tokens *current;
+	int i;
+	
+	i = 1;
+	memory->tokens = create_token(av[i++], memory);
+	current = memory->tokens;
+	while(i < ac)
+	{
+		current->next = create_token(av[i], memory);
+		current = current->next;
+		if(!current)
+			return(NULL);
+		i++;
+	}
+	return (1);
+}
+
+int	main(int ac, char **av)
+{
+	t_memory *memory;
+	memory = malloc(sizeof(memory));
+	if (!memory)
+		return(ERROR);
+	lexer(ac, av, memory);
+	if (!memory->tokens)
+		return(ERROR);
 	return (SUCCESS);
 }
