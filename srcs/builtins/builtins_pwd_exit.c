9eebe231180113b4bf940a960abbfb9ed668dbf6
timofeykafanov <tim.kafanov@gmail.com>
2024-11-19 21:49:56 +0100

    feat: complete exit command

--- srcs/builtins/builtins_pwd_exit.c
+++ srcs/builtins/builtins_pwd_exit.c
@@ -6,7 +6,7 @@
 /*   By: tkafanov <tkafanov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 15:30:54 by tkafanov          #+#    #+#             */
-/*   Updated: 2024/11/19 19:43:03 by tkafanov         ###   ########.fr       */
+/*   Updated: 2024/11/19 21:39:34 by tkafanov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,47 @@ void	execute_pwd(t_memory *memory)
 	if (memory->pwd != NULL)
 		ft_printf("%s\n", STDOUT_FILENO, memory->pwd);
 	else
-		perror("minishell: pwd");
+		perror("pwd");
+}
+
+static bool	contains_only_digits(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (false);
+		i++;
+	}
+	return (true);
 }
 
 void	execute_exit(t_memory *memory, bool is_redir, int saved_fds[2])
 {
+	t_command	*command;
+
+	command = memory->commands;
+	if (command->args[1] && command->args[2])
+	{
+		ft_printf("exit\n", STDERR_FILENO);
+		ft_printf("exit: too many arguments\n", STDERR_FILENO);
+		memory->exit_status = 1;
+		return ;
+	}
+	else if (command->args[1])
+	{
+		if (contains_only_digits(command->args[1]))
+			memory->exit_status = ft_atoi(command->args[1]) % 256;
+		else
+		{
+			ft_printf("exit\n", STDERR_FILENO);
+			ft_printf("exit: %s: numeric argument required\n", \
+				STDERR_FILENO, command->args[1]);
+			memory->exit_status = 2;
+		}
+	}
 	if (is_redir && saved_fds)
 	{
 		dup2(saved_fds[0], STDIN_FILENO);
@@ -32,5 +68,5 @@ void	execute_exit(t_memory *memory, bool is_redir, int saved_fds[2])
 	}
 	free_memory(memory);
 	// ft_printf("exit\n", STDOUT_FILENO);
-	exit(0);
+	exit(memory->exit_status);
 }
