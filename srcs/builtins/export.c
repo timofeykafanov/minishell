25cbaaf43fc584c536ead505f9341deeff02a9a3
timofeykafanov <tim.kafanov@gmail.com>
2024-12-27 13:19:26 +0100

    fix export

--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -6,13 +6,13 @@
 /*   By: tkafanov <tkafanov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 15:41:19 by tkafanov          #+#    #+#             */
-/*   Updated: 2024/12/24 13:31:19 by tkafanov         ###   ########.fr       */
+/*   Updated: 2024/12/25 21:41:21 by tkafanov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static void	replace(t_memory *memory, char **args, int j, char *var_name)
+static void	replace(t_memory *memory, char *arg, char *var_name)
 {
 	int		i;
 	size_t	env_var_len;
@@ -25,7 +25,7 @@ static void	replace(t_memory *memory, char **args, int j, char *var_name)
 			ft_strlen(var_name)) == 0 && env_var_len == ft_strlen(var_name))
 		{
 			free(memory->env[i]);
-			memory->env[i] = ft_strdup(args[j]);
+			memory->env[i] = ft_strdup(arg);
 			if (!memory->env[i])
 			{
 				free(var_name);
@@ -38,21 +38,26 @@ static void	replace(t_memory *memory, char **args, int j, char *var_name)
 	}
 }
 
-void	find_var(t_memory *memory, char **args, int j)
+static void	find_var(t_memory *memory, char *arg)
 {
 	char	*var_name;
+	char	*has_value;
 
-	if (ft_strchr(args[j], '='))
-		var_name = ft_strncpy(args[j], \
-		ft_strchr(args[j], '=') - args[j]);
+	if (ft_strchr(arg, '='))
+		var_name = ft_strncpy(arg, \
+		ft_strchr(arg, '=') - arg);
 	else
-		var_name = ft_strdup(args[j]);
+		var_name = ft_strdup(arg);
 	if (!var_name)
 	{
 		free_memory(memory);
 		exit(ERROR);
 	}
-	replace(memory, args, j, var_name);
+	has_value = ft_strchr(arg, '=');
+	if (has_value)
+		replace(memory, arg, var_name);
+	else
+		memory->found = true;
 	free(var_name);
 }
 
@@ -81,7 +86,7 @@ static void	add_env_var(t_memory *memory, char **args)
 	while (args[j])
 	{
 		memory->found = false;
-		find_var(memory, args, j);
+		find_var(memory, args[j]);
 		if (memory->found)
 		{
 			j++;
