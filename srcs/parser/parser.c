1aae0388117e27dafff03e81236d97ab47ebe062
Silas Oppermann <silas.oppermann@gmail.com>
2025-01-04 16:21:04 +0100

    fixed first token is whitesapce in parser phase 1

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tkafanov <tkafanov@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sopperma <sopperma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 12:04:10 by tkafanov          #+#    #+#             */
-/*   Updated: 2024/12/25 20:06:25 by tkafanov         ###   ########.fr       */
+/*   Updated: 2025/01/04 16:19:59 by sopperma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,8 @@ void	parser_phase_one_loop(t_parser *p)
 
 void	parser_phase_one(t_parser *p, t_memory *memory)
 {
+	if (p->current_token->type == T_WHITESPACE && p->current_token->next)
+		p->current_token = p->current_token->next;
 	if (is_redirect(p->current_token->type))
 		p->current_cmd = create_command("", NULL, p->current_token->type \
 			, memory);
